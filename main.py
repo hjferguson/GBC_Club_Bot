1944467e4ac1973c60e94623f5a8d7c65b635af6
Harlan Ferguson <harlanjazz@gmail.com>
2023-09-20 22:19:03 -0400

    adds basic working functionality

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
+import os
+from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
+# Load environment variables
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix='!')
+# Define your bot's intents
+intents = discord.Intents.default()
+intents.members = True  # Enable the intent to track member joins
+
+# Create a bot instance with intents
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
 
 @bot.event
 async def on_member_join(member):
-    # Send a salute emoji to the new member
-    welcome_channel = member.guild.text_channels[0]  # Replace with your desired channel
-    await welcome_channel.send(f"Welcome {member.mention}! 🎉")
+    # Find the welcome channel (by name or ID)
+    welcome_channel = discord.utils.get(member.guild.text_channels, name='welcome')
 
+    if welcome_channel:
+        # Get the last message in the welcome channel (which is the welcome message)
+        async for message in welcome_channel.history(limit=1):
+            # React to the welcome message with a salute emoji
+            await message.add_reaction('🫡')
+
+# Run your bot
+bot.run(DISCORD_TOKEN)
